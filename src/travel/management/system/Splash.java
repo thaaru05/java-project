370d974b704c319ee52662745d8d859cfb04862e
thaaru05 <thaaruk.s@gmail.com>
2023-12-07 15:33:29 +0530

    2 vid

---
+++ src/travel/management/system/Splash.java
@@ -0,0 +1,20 @@
+package travel.management.system;
+import javax.swing.*;
+import java.awt.*;
+public class Splash extends JFrame {
+    Splash(){
+        setSize(1200,600);
+        setLocation(200,100);
+        
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/splash.jpg"));
+        Image i2=i1.getImage().getScaledInstance(1200,600,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image =new JLabel(i3);
+        add(image);
+        
+        setVisible(true);
+    }
+    public static void main(String args[]){
+       Splash frame= new Splash();
+    }
+}
